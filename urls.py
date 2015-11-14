ab0f9a0ae6fe8f54e238654bf1aa1451c945a5bb
ewui <ewui>
2015-11-15 00:05:38 +0800

    initial commit

---
+++ urls.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#frbe
+"""
+Created on Fri Dec 06 16:54:36 2013
+
+@author: Zengliangwei
+"""
+from django.conf.urls import patterns, url
+from addr_book.views import *
+
+urlpatterns = patterns('',
+    (r'^site_media/(?P<path>.*)','django.views.static.serve',{'document_root':'./addr_book/'}), 
+    url(r'^$',main_page),
+    url(r'^addpeople/',add_people),
+    url(r'^check/',check),
+    url(r'^delete',delete),
+    url(r'^update',update)
+)
